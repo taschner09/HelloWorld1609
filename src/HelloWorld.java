b97a760e229bcadfacd08710624b39845648b028
Lisa Taschner <52208480@edu.campus02.at>
2022-09-16 19:27:48 +0200

    Wir sind für heute mit KONF fertig.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
         System.out.println("Welcome to KONF 2022!");
         System.out.println("Zeile 3");
         System.out.println("Zeile 4"); //STRG+D
+        System.out.println("Zeile 5");
 
     }
 }
