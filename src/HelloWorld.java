1dc6f7b24bee870e379e4055d8334820767447b0
Lisa Taschner <52208480@edu.campus02.at>
2022-09-16 19:12:42 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    //main+TAB
+    //psvm+TAB
+    public static void main(String[] args) {
+        //sout+TAB
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); //STRG+D
+
+    }
+}
